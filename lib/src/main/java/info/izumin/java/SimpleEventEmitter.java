c3c838bfd021d781b08daaa015352290799ba977
izumin5210 <masayuki@izumin.info>
2015-09-07 02:06:28 +0900

    Implement SimpleEventEmitter

--- lib/src/main/java/info/izumin/java/SimpleEventEmitter.java
+++ lib/src/main/java/info/izumin/java/SimpleEventEmitter.java
@@ -1,4 +1,26 @@
 package info.izumin.java;
 
-public class SimpleEventEmitter {
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+
+public class SimpleEventEmitter<K extends Enum<K>, V> {
+    private EnumMap<K, List<SimpleEventListener<V>>> mListeners;
+
+    public SimpleEventEmitter(Class<K> enumType) {
+        mListeners = new EnumMap<>(enumType);
+    }
+
+    public void on(K key, SimpleEventListener<V> listener) {
+        if (!mListeners.containsKey(key)) {
+            mListeners.put(key, new ArrayList<SimpleEventListener<V>>());
+        }
+        mListeners.get(key).add(listener);
+    }
+
+    public void emit(K key, V value) {
+        for (SimpleEventListener<V> listener : mListeners.get(key)) {
+            listener.run(value);
+        }
+    }
 }
